a30238ca741b1757e1cb540c7d404a78f0521acd
Aguinaldo Neto <neto_aguinaldo@outlook.com>
2024-03-23 11:20:40 -0300

    Criando o service para postar um login

--- front-end/login-page/src/app/pages/login/login.component.ts
+++ front-end/login-page/src/app/pages/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +14,9 @@ import { PrimaryInputComponent } from '../../components/primary-input/primary-in
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -19,11 +25,28 @@ export class LoginComponent {
   // a exclamação indica que em algum momento eu vou declarar em algum momento, agora tá vazio, mas no construtor eu vou declarar ele.
   loginForm!: FormGroup;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+    ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     });
+  }
+
+  submit() {
+    this.loginService
+      .login(this.loginForm.value.email, this.loginForm.value.password)
+      .subscribe({
+        next: () => this.toastService.success("Login feito com sucesso"),
+        error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde.")
+      })
+  }
+
+  navigate() {
+    this.router.navigate(["signup"]);
   }
 
 }
