4ec161eb911c45088072b4685d84b492e63b03a6
Aguinaldo Neto <neto_aguinaldo@outlook.com>
2024-04-14 19:35:45 -0300

    Ajustes finais do front

--- front-end/login-page/src/app/services/login.service.ts
+++ front-end/login-page/src/app/services/login.service.ts
@@ -8,10 +8,12 @@ import { tap } from 'rxjs';
 })
 export class LoginService {
 
+  apiUrl: string = 'http://localhost:8080/auth';
+
   constructor(private httpClient: HttpClient) { }
 
   login(email: string, password: string) {
-    return this.httpClient.post<LoginResponse>("/login", {email, password})
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", {email, password})
     .pipe(
       tap( (value) => {
         sessionStorage.setItem("auth-token", value.token)
@@ -19,4 +21,15 @@ export class LoginService {
       })
     );
   }
+
+  signup(name: string, email: string, password: string) {
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", {name, email, password})
+    .pipe(
+      tap( (value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    );
+  }
+
 }
